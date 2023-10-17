367b8426c4bca1329ffd8839c16e9dcb70edc3e0
laprinia <lavinialoghin7@gmail.com>
2023-10-17 18:25:58 +0300

    Added Base Menu Scene

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static bool isContinue;
+    public void NewGame()
+    {
+        StartCoroutine(LoadAsyncScene());
+    }
+    public void ContinueGame()
+    {
+        isContinue = true;
+        StartCoroutine(LoadAsyncScene());
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Ovidiu");
+
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    //
+}
+
---
+++ Assets/Scripts/UI/Menu/PauseMenu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject playerComponent;
+    public static bool GameIsPaused = false;
+    public GameObject saveMenuUI;
+    public GameObject pauseMenuUI;
+    public GameObject inventoryCanvas;
+   // public GameObject dialogueCanvas;
+   
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        } 
+    }
+
+    public void Resume()
+    {
+        Cursor.visible = false;
+        saveMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+       // dialogueCanvas.SetActive(true);
+        inventoryCanvas.SetActive(false);
+    }
+
+    void Pause()
+    {
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+       
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        //dialogueCanvas.SetActive(false);
+        inventoryCanvas.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SaveGame()
+    {
+        //TODO
+    }
+
+    public void LoadGame()
+    {
+        //TODO
+    }
+}
